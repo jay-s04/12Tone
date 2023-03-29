3cb3fb5a7e5446dda1a6cb927d34c0b52ee46116
Jay <80469071+jay-s04@users.noreply.github.com>
2023-03-29 10:03:04 -0400

    Edited main and README main is completely functional with minor bugs Added some things to README

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import time
 
 # We need to get a few things from the user to start with: reference note, prime series and transformation
 def get_reference_pitch():
-    base_list = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']
+    while True:
+        base_list = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']
 
-    reference_pitch = input("Please enter your reference pitch: ")
+        reference_pitch = input("Please enter your reference pitch: ")
 
-    if reference_pitch in base_list:
-        print(f"Your reference pitch is {reference_pitch}")
-        return reference_pitch
-    else:
-        print("Invalid Reference Pitch. Available accidentals include: C#, Eb, F#, Ab, Bb\n")
-        get_reference_pitch()
+        if reference_pitch in base_list:
+            print(f"Your reference pitch is {reference_pitch}")
+            return reference_pitch
+        else:
+            print("Invalid Reference Pitch. Available accidentals include: C#, Eb, F#, Ab, Bb\n")
+        
 
 
 # Based on the reference pitch, we are able to create a corresponding dictionary.
@@ -34,33 +35,37 @@ def create_note_dictionary(reference_pitch: str):
 
 # Now to get the prime series from the user
 def get_prime_series():
-    user_input = input("Please enter your 12tone series. Seperate the numbers with spaces: ")
-    series = [i for i in list(user_input.split(' '))]
-    # And now we check for errors.
-    # Our two main checks are whether each element of the list is a number and whether its less than 12
-    # This check also converts the list[str] to a list[int]
-    for index, element in enumerate(series):
-        if element.isdigit():
-            if int(element) < 12:
-                series[index] = int(element)
+    while True:
+        user_input = input("Please enter your 12tone series. Seperate the numbers with spaces: ")
+        series = [i for i in list(user_input.split(' '))]
+        # And now we check for errors.
+        # Our two main checks are whether each element of the list is a number and whether its less than 12
+        # This check also converts the list[str] to a list[int]
+        for index, element in enumerate(series):
+            if element.isdigit():
+                if int(element) < 12:
+                    series[index] = int(element)
+                else:
+                    print('Each element must be below 12\n')
+                    continue
             else:
-                print('Each element must be below 12\n')
-                get_prime_series()
+                print("Each element must be an integer from 0 - 11\n")
+                continue
+        # Our last check is to see whether the series has 12 unique tones
+        if len(series) == 12 and (len(series) == len(set(series))):
+            print(f"Your series is {series}")
+            return series
         else:
-            print("Each element must be an integer from 0 - 11\n")
-            get_prime_series()
-    # Our last check is to see whether the series has 12 unique tones
-    if len(series) == 12 and (len(series) == len(set(series))):
-        print(f"Your series is {series}")
-        return series
-    else:
-        print('There should be 12 unique tones in your series\n')
-        get_prime_series()
+            print('There should be 12 unique tones in your series\n')
+            continue
 
 # Now to write the three possible transformations
-def transpose(prime_series: list[int], n):
+def transpose(prime_series: list[int], n: int):
     # The modulus 12 is to keep everything between 0 and 11
-    return list((i + n) % 12 for i in prime_series) 
+    transposed_list = []
+    for i in prime_series:
+        transposed_list.append((i + n) % 12)
+    return transposed_list
 def invert(prime_series: list[int]):
     inverted_list = []
     for i in prime_series:
@@ -74,31 +79,55 @@ def retrograde(prime_series: list[int]):
 
 # Finally, we can get what transformation the user wants to do to their series
 def transform(prime_series: list[int]):
-    transformations = input("What transformations would you like to do to your series: ")
-    transformations = transformations.lower()
-    new_list = prime_series.copy()
-    # Now to check and apply the transformations.
-    """
-    for i in transformations:
-        print(new_list)
-        if i.lower() == 'r':
-            new_list = retrograde(new_list)
-            print(new_list)
-        elif i.lower() == 'i':
-            new_list = invert(new_list)
-        elif str.isdigit(i):
-            new_list = transpose(new_list, int(i))
-        elif i.lower() == 'p':
-            new_list = new_list
-            pass
-        else:
-            print('Error')
-            transform(prime_series) # This shouldn't happen unless I need it to
+    while(True): # This is necessary to get transformations over and over until they work
+        transformations = input("What transformations would you like to do to your series: ")
+        new_series = prime_series.copy()
+        error = False
 
-    return new_list
+        # Put something in here to deal with if they enter the number before
+        # the letter. Something like '4R' instead of 'R4'
+        # This is because it refers back to the instantiated dictionary instead 
+        # of creating a new one in this case.
 
-print(transform([0, 1, 2, 3]))
-"""
-# TODO: Make the loop work. I have no idea why it goes to the else and throws the error
-# even after I reset the variable values.
-# Honestly I might just rewrite the loop to make it work properly
+        # Now to check and apply the transformations.
+        for i in transformations:
+            if i.isdigit():
+                new_series = transpose(new_series, int(i))
+                print(i)
+            elif i.lower() == 'r':
+                new_series = retrograde(new_series)
+            elif i.lower() == 'i':
+                new_series = invert(new_series)
+            elif i.lower() == 'p':
+                pass
+            else:
+                print('Error')
+                error = True
+                continue # If there's an error at all, it will break out of the initial for loop
+
+        if error == False: # Checks if there was an error that broke out of the loop
+            return new_series
+
+# And now just a little something to spit out the notes corresponding to the numbers
+def spit_out_notes(new_series, note_dictionary):
+    notes = ''
+    for i in new_series:
+        notes += note_dictionary[i] + ' '
+    return notes
+
+# Now to put it all together.
+# The order is: get the reference pitch, then create a dictionary on it, then get the prime series,
+# then get the transformations and apply them. Finally, spit out the notes for the series.
+
+def run():
+    reference_pitch = get_reference_pitch()
+    note_dictionary = create_note_dictionary(reference_pitch)
+    prime_series = get_prime_series()
+    new_series = transform(prime_series)
+    notes = spit_out_notes(new_series, note_dictionary)
+    print(notes)
+
+run()
+# TODO: Add a while(True) to the run() function with a y/n break if the user
+# wants to run the program again. Also add a conditional for if they want
+# to use the same series they entered last time.
